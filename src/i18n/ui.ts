2b04ee86a07060a14925ce01930e33a45a8e2f94
number44 <daniel.lesiewicz.dev@gmail.com>
2023-09-25 19:48:24 +0200

    first

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  en: "English",
+  pl: "Polish",
+};
+
+export const defaultLang = "pl";
+
+export const ui = {
+  pl: {
+    "nav.twitter": "Twitter",
+    "nav.home": "Home",
+    "nav.about": "O mnie",
+  },
+  es: {
+    "nav.home": "Homees",
+    "nav.about": "O mniees",
+  },
+
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+} as const;
+
+export const showDefaultLang = false;
